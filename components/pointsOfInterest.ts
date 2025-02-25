9417b97f775dcb30b033918c7bac01a9e83643fa
otavio.silva <otavioaraujo490@gmail.com>
2025-02-25 16:02:00 -0300

    feat: add points of interest data for 3D visualization

---
+++ components/pointsOfInterest.ts
@@ -0,0 +1,18 @@
+import * as THREE from "three";
+
+export const pointsOfInterest = [
+  {
+    position: new THREE.Vector3(6, 1, 3),
+    title: "Condomínio das Torres Reais",
+    description:
+      "Localizado em uma das áreas mais nobres da cidade, o Condomínio das Torres Reais combina sofisticação, segurança e conforto em um único espaço. Projetado para oferecer uma experiência de moradia diferenciada, o empreendimento é composto por três imponentes torres de arquitetura moderna e elegante, cercadas por uma infraestrutura completa que valoriza a qualidade de vida dos moradores.",
+    image: "/ponto1.jpg",
+  },
+  {
+    position: new THREE.Vector3(-8, 1, 1),
+    title: "Escola Municipal Novo Saber",
+    description:
+      "A Escola Municipal Novo Saber é uma instituição de ensino comprometida com a formação acadêmica, social e emocional de seus alunos. Localizada em um ambiente seguro e acolhedor, a escola oferece uma educação de qualidade voltada para o desenvolvimento integral das crianças e adolescentes, preparando-os para os desafios do futuro.",
+    image: "/ponto2.jpg",
+  },
+];
