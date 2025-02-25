9bb960d7f77529c292d68e596690c496fa705479
Hugo Rodrigues <hugocavalcantesr@gmail.com>
2025-02-25 10:32:11 -0300

    feat: removed loading text from inside loader animation

--- components/threeScene.tsx
+++ components/threeScene.tsx
@@ -137,7 +137,7 @@ const ThreeScene = () => {
     <>
       {isLoading && (
         <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
-          <div className="loader">Loading...</div>
+          <div className="loader"></div>
         </div>
       )}
       <div ref={mountRef} className="w-full h-full" />
